46c3bb0743ecb36cf81fd25b68bec437d74b61ef
Anil <yadavanil233@gmail.com>
2023-04-07 15:58:55 +0545

    initial commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { Context } from '../main'
+import Loader from '../components/Loader'
+
+const Profile = () => {
+    const { isAuthenticated, loading, user } = useContext(Context)
+    return (
+        loading ? <Loader /> : (
+            <div>
+                <h1>Name: {user.name}</h1>
+                <p>Email: {user?.email}</p>
+            </div>
+        )
+    )
+}
+
+export default Profile
